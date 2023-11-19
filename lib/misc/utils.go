fdc85eb30731e9ef16f1b7a1be66adc002d3cd4b
sammy1song@utexas.edu <sammy1song@utexas.edu>
2023-11-19 15:50:51 -0600

    SPORT-137: Fixed errors + comments

--- lib/misc/utils.go
+++ lib/misc/utils.go
@@ -5,9 +5,7 @@ import "encoding/json"
 func PickFields(data any, fields ...string) map[string]any {
 	bytes, _ := json.Marshal(&data)
 	fullMap := make(map[string]any)
-	//lint comment used to bycheck linter because format was correct for code
-
-	json.Unmarshal(bytes, &fullMap)
+	_ = json.Unmarshal(bytes, &fullMap)
 	result := make(map[string]any, len(fields))
 	for _, f := range fields {
 		if v, ok := fullMap[f]; ok {
