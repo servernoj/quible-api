0185936040705a8dc8510dd3ec1b9f4fcc7395a9
sammy1song@utexas.edu <sammy1song@utexas.edu>
2023-11-19 00:51:25 -0600

    SPORT-134: Added unit tests utils_test.go

---
+++ lib/misc/utils_test.go
@@ -0,0 +1,49 @@
+package misc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPickFields(t *testing.T) {
+	tests := []struct {
+		name   string
+		data   interface{}
+		fields []string
+		want   map[string]interface{}
+	}{
+		{
+			name: "Pick single field",
+			data: struct {
+				ID   int
+				Name string
+			}{ID: 1, Name: "Test"},
+			fields: []string{"Name"},
+			want:   map[string]interface{}{"Name": "Test"},
+		},
+		{
+			name: "Pick multiple fields",
+			data: struct {
+				ID    int
+				Name  string
+				Email string
+			}{ID: 2, Name: "John", Email: "john@example.com"},
+			fields: []string{"ID", "Email"},
+			want:   map[string]interface{}{"ID": 2, "Email": "john@example.com"},
+		},
+		{
+			name:   "Pick non-existent field",
+			data:   struct{ Name string }{Name: "Jane"},
+			fields: []string{"Age"},
+			want:   map[string]interface{}{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PickFields(tt.data, tt.fields...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PickFields() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
