a49d3b53b52c38db50946a0cc08fb95567dc15ed
Simon Baev <simon.baev@bsci.com>
2023-11-22 15:56:03 -0700

    SPORT-139: fix unit tests for jwt

--- auth-service/controller/jwt_test.go
+++ auth-service/controller/jwt_test.go
@@ -2,21 +2,19 @@ package controller
 
 import (
 	"os"
-	"reflect"
 	"testing"
-	"time"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/quible-backend/lib/models"
 )
 
-// unitest for the jwt.go generateToken test
+// unit test for the jwt.go generateToken test
 func TestGenerateToken(t *testing.T) {
 	// set the ENV_JWT_SECRET
 	os.Setenv("ENV_JWT_SECRET", "your_test_jwt_secret")
 
-	// define the tesycases struct
+	// define the test cases struct
 	testCases := []struct {
 		name string
 		user *models.User
@@ -37,28 +35,21 @@ func TestGenerateToken(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			token := generateToken(tc.user)
-			assert.NotEmpty(t, token, "Token should not be empty")
+			// false here to indicate "access" (not "refresh") flavor of the token
+			token, _ := generateToken(tc.user, false)
+			assert.NotEmpty(t, token.Token, "Token should not be empty")
 
 			// The generated token is parsed and validated using jwt.ParseWithClaims.
 			//This ensures the token is correctly structured and valid.
-			parsedToken, err := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+			parsedToken, err := jwt.ParseWithClaims(token.Token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
 				return []byte("your_test_jwt_secret"), nil
 			})
 			assert.NoError(t, err, "Token should be valid")
 
 			// Claims Verification
 			if claims, ok := parsedToken.Claims.(*MyClaims); ok && parsedToken.Valid {
-				expectedClaims := MyClaims{
-					StandardClaims: jwt.StandardClaims{
-						Issuer:    APPLICATION_NAME,
-						ExpiresAt: time.Now().Add(LOGIN_EXPIRATION_DURATION).Unix(),
-					},
-					ID:    tc.user.ID,
-					Email: tc.user.Email,
-				}
-				// use reflect.DeepEqual
-				assert.True(t, reflect.DeepEqual(expectedClaims, *claims), "Claims should match expected values")
+				assert.Equal(t, claims.UserId, tc.user.ID)
+				assert.Equal(t, claims.Email, tc.user.Email)
 			} else {
 				t.Fatal("Failed to parse token claims or token is not valid")
 			}
@@ -70,7 +61,7 @@ func TestVerifyJWT(t *testing.T) {
 	// set the ENV_JWT_SECRET
 	os.Setenv("ENV_JWT_SECRET", "your_test_jwt_secret")
 
-	// define the testcases
+	// define the test cases
 	testCases := []struct {
 		name        string
 		user        *models.User
@@ -95,17 +86,20 @@ func TestVerifyJWT(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var tokenString string
+			var generatedToken GeneratedToken
 			if tc.user != nil {
 				// generate useful token
-				tokenString = generateToken(tc.user)
+				generatedToken, _ = generateToken(tc.user, false)
 			} else {
 				// generate unuseful token
-				tokenString = "invalid.token.string"
+				generatedToken = GeneratedToken{
+					Token: "invalid.token.string",
+					ID:    "invalid.id",
+				}
 			}
 
 			// run verifyJWT function
-			ID, err := verifyJWT(tokenString)
+			claims, err := verifyJWT(generatedToken.Token, false)
 
 			if tc.expectError {
 				// assert should have faults
@@ -113,7 +107,8 @@ func TestVerifyJWT(t *testing.T) {
 			} else {
 				// assert should success, have no faults
 				assert.NoError(t, err, "No error expected for valid token")
-				assert.Equal(t, tc.user.ID, ID, "ID should match the user ID")
+				userId := claims["userId"].(string)
+				assert.Contains(t, tc.user.ID, userId, "userId from claims should match the user ID")
 			}
 		})
 	}
