8de8592939765ffce903d4a7bc4cfc816d203cf2
Simon Baev <simon.baev@bsci.com>
2023-12-18 14:35:31 -0700

    SPORT-168: allow for unprotected jive

--- cmd/run.sh
+++ cmd/run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Usage: ./run.sh <command> <args>
+# with <command> being a sub-directory of `cmd` with compilable `main.go`
+#      <args> being command specific arguments, see below example
+#
+# For example, to migrate DB to the latest version run `./run.sh migrate up`
+
 ROOT=$(dirname ${0})
 CMD=$1
 
