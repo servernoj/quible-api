f762c0ef9eea183e45b5812b223e6dee2f2225c7
Simon Baev <simon.baev@bsci.com>
2023-12-25 15:22:08 -0700

    add Swagger for /send in mail-service

--- mail-service/controller/handlers.go
+++ mail-service/controller/handlers.go
@@ -8,6 +8,18 @@ import (
 	"github.com/quible-io/quible-api/mail-service/postmark"
 )
 
+// @Summary		Send single email
+// @Description	Use 3rd party (Postmark) mail delivery service to send an email
+// @Tags			private
+// @Accept		json
+// @Produce		json
+// @Param			request	body		postmark.EmailDTO	true	"Email delivery request"
+// @Success		200		{object}	postmark.PostmarkResponse
+// @Failure		400		{object}	ErrorResponse
+// @Failure		401		{object}	ErrorResponse
+// @Failure		424		{object}	ErrorResponse
+// @Failure		500		{object}	ErrorResponse
+// @Router		/send [post]
 func SendEmailHandler(c *gin.Context) {
 
 	type DTO = postmark.EmailDTO
