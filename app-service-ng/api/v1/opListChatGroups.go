642fb0b5233743dbfcab11c7d14d9eda105118bb
Simon Baev <simon.baev@bsci.com>
2024-02-17 17:08:05 -0700

    SPORT-225: add list chat groups operation

--- app-service-ng/api/v1/opListChatGroups.go
+++ app-service-ng/api/v1/opListChatGroups.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/danielgtaylor/huma/v2"
 	libAPI "github.com/quible-io/quible-api/lib/api"
+	"github.com/quible-io/quible-api/lib/models"
+	"github.com/volatiletech/null/v8"
 )
 
 type ListChatGroupsInput struct {
@@ -13,6 +15,7 @@ type ListChatGroupsInput struct {
 }
 
 type ListChatGroupsOutput struct {
+	Body models.ChatSlice
 }
 
 func (impl *VersionedImpl) RegisterListChatGroups(api huma.API, vc libAPI.VersionConfig) {
@@ -33,7 +36,18 @@ func (impl *VersionedImpl) RegisterListChatGroups(api huma.API, vc libAPI.Versio
 			},
 		),
 		func(ctx context.Context, input *ListChatGroupsInput) (*ListChatGroupsOutput, error) {
-			return nil, nil
+			chatGroups, err := models.Chats(
+				models.ChatWhere.ParentID.IsNull(),
+				models.ChatWhere.OwnerID.EQ(null.StringFrom(input.UserId)),
+			).AllG(ctx)
+			if err != nil {
+				return nil, ErrorMap.GetErrorResponse(
+					Err500_UnknownError,
+				)
+			}
+			return &ListChatGroupsOutput{
+				Body: chatGroups,
+			}, nil
 		},
 	)
 }
